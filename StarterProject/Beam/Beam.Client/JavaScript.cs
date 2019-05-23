5fc6cdd9f20cba70948e5885566d13c55b13327f
sapatelbaps <sapatelbaps@gmail.com>
2019-05-22 20:15:39 -0700

    Beam added in the settings page. Added to JS to create that beam. Called functions of JS file from razor page. Bind event of JavaScript.cs file with JS function and page properties bindings. Though it will overflow due to rerendering beams repeatedly via "StatehasChanged" event in the settings.cshtml page.

---
+++ StarterProject/Beam/Beam.Client/JavaScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Beam.Client
+{
+    public static class JavaScript
+    {
+        public static event Action BeamPassTriggered;
+
+        public static Task LoadAnimation(string elementId, int width, int height)
+        {
+            return JSRuntime.Current.InvokeAsync<object>("animatedBeam.loadAnimation",
+                elementId, width, height);
+        }
+
+        [JSInvokable]
+        public static Task BeamPassedBy()
+        {
+            return Task.Run(() => BeamPassTriggered?.Invoke());
+        }
+
+    }
+}
